856ab7cadb34e0e4198098b1e18243bbe8fbc39b
marihachi <marihachi0620@gmail.com>
2021-08-21 16:39:15 +0900

    basic .wat code generation

--- src/basic/index.ts
+++ src/basic/index.ts
@@ -2,13 +2,91 @@ import { promises as fs } from 'fs';
 import path from 'path';
 import { Wat2Wasm } from '../wat2wasm';
 
+type TypeName = string;
+type ParamInfo = { type: TypeName };
+
+class Func {
+	name?: string;
+	paramInfos: ParamInfo[] = [];
+	resultType?: TypeName;
+	bodyItems: string[] = [];
+
+	constructor() {
+	}
+
+	generate() {
+		const headerItems: string[] = [];
+		if (this.name) {
+			headerItems.push(`$${ this.name }`);
+		}
+		const paramItems = this.paramInfos.map(info => {
+			const items: string[] = [];
+			items.push(info.type);
+			return `(param ${ items.join(' ') })`;
+		});
+		headerItems.push(paramItems.join(' '));
+		if (this.resultType) {
+			headerItems.push(`(result ${ this.resultType })`);
+		}
+
+		return `(func ${ headerItems.join(' ') } ${ this.bodyItems.join(' ') })`;
+	}
+}
+
+function genFuncImport(moduleName: string, externalName: string, internalName: string) {
+	return `(import "${ moduleName }" "${ externalName }" (func $${ internalName }))`;
+}
+
+function genFuncExport(externalName: string, internalName: string) {
+	return `(export "${ externalName }" (func $${ internalName }))`;
+}
+
+function genCall(name: string) {
+	return `call $${name}`;
+}
+
+function genModule(items: string[]) {
+	return `(module ${ items.join(' ') })\n`;
+}
+
 async function entry() {
 
-	// load .wat file
-	const wat = await fs.readFile(path.resolve(__dirname, '../../src/basic/basic.wat'));
+	const moduleItems: string[] = [];
+
+	// import a function from js
+	moduleItems.push(genFuncImport('basic', 'fn', 'f'));
+
+	// function
+	const func = new Func();
+
+	// function: set signature
+	func.name = 'run';
+	func.paramInfos.push(...[
+		{ type: 'i32' },
+		{ type: 'i32' },
+	]);
+	func.resultType = 'i32';
+
+	// function: set statements
+	func.bodyItems.push(...[
+		genCall('f')
+	]);
+	func.bodyItems.push('i32.const 1');
+	func.bodyItems.push('return');
+
+	// function: generate
+	moduleItems.push(func.generate());
+
+	// export the run function
+	moduleItems.push(genFuncExport('run', 'run'));
+
+	const wat = genModule(moduleItems);
 
 	const wat2Wasm = await Wat2Wasm.new();
 	const wasm = wat2Wasm.generate(wat);
+
+	// write .wat file
+	await fs.writeFile(path.resolve(__dirname, './basic.wat'), wat);
 
 	// write .wasm file
 	await fs.writeFile(path.resolve(__dirname, './basic.wasm'), wasm);
@@ -27,7 +105,8 @@ async function entry() {
 
 	// call exported function
 	if (typeof instance.exports.run == 'function') {
-		console.log('run:', instance.exports.run(1, 2));
+		//console.log('run:', instance.exports.run(1, 2));
+		console.log('run:', instance.exports.run());
 	}
 }
 
